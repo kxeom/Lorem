7ab78b8e04dffc138f14956a95d2901343ab4533
Akira <87409484+kxeom@users.noreply.github.com>
2024-11-26 14:11:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let gallery = document.querySelector(".gallery-box");
+let spacingBtn = document.querySelector(".spaceBtn")
+spacingBtn.addEventListener('click',()=>{
+    gallery.classList.toggle('active');
+})
+
+function setCookie(cname, cvalue, exdays) {
+    let d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(const element of ca) {
+      let c = element;
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+  }
